f4dd77797455eaf3495a63ffd25ab6ab68292862
rogeriopvl <rogeriopvl@gmail.com>
2011-08-25 01:17:58 +0100

    Some code testing the calendar thingy

--- lib/scp_homey/scrapper.rb
+++ lib/scp_homey/scrapper.rb
@@ -4,9 +4,13 @@ require 'open-uri'
 
 module SCPHomey
   
+  # Exception to raise when an error occurs scrapping the site
+  class UnableToParseDataError < Exception; end
+
   class Scrapper
 
     # page that lists the next SCP games
+    USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.215 Safari/535.1'
     NEXT_GAMES_URL = 'http://www.zerozero.pt/equipa.php?grp=0&ond=c&compet_id_jogos=0&epoca_id=141&id=16&menu=nextmatches&type=season'
 
     def initialize
@@ -14,18 +18,24 @@ module SCPHomey
     end
 
     def run
-      tds = Array.new
-      doc = Nokogiri::HTML(open(NEXT_GAMES_URL))
+      doc = Nokogiri::HTML(open(NEXT_GAMES_URL, 'User-Agent' => USER_AGENT))
       doc.css('table.statsn tr').each do |item|
+        tds = Array.new
         item.css('td').each do |td|
           tds << td.text
         end
-        if tds.length > 0
+        unless tds.empty?
+          # index 0 contains date, 5 the opponent and 6 the competition
           game = SCPHomey::Game.new(tds[0], tds[5], tds[6])
           @games << game
         end
       end
-      puts @games.inspect
+      
+      if @games.empty?
+        raise UnableToParseDataError
+      end
+
+      return @games
     end
 
   end 
--- test/test.rb
+++ test/test.rb
@@ -1,4 +1,17 @@
 require '../lib/scp_homey.rb'
+require 'rubygems'
+require 'gcal4ruby'
 
-scp = SCPHomey::Scrapper.new()
-scp.run
+scp = SCPHomey::Scrapper.new
+games = scp.run
+cal = SCPHomey::Calendar.new
+cal.delete_all_events
+
+games.each do |game|
+  unless ['Sunday', 'Saturday'].include?(game.date.strftime("%A"))
+    puts game.date
+    cal.create_event(game.date)
+  end
+end
+
+
